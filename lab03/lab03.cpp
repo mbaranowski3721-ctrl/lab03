8c1457ca1ea4feb1e200346239ada0b3b2fbe3cd
mbaranowski3721-ctrl <m.baranowski3721@gmail.com>
2025-10-21 15:29:25 +0200

    Dodaj pliki projektów.

---
+++ lab03/lab03.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b;
+    cout << "Wprowadź pierwszą zmienną: ";
+    cin >> a;
+    cout << "Wprowadź drugą zmienną: ";
+    cin >> b;
+    if (a == b) {
+        cout << "Liczby są równe";
+    }
+    else if(a<b) {
+        cout << "Zmienna a jest mniejsza od zmiennej b";
+    }
+    else {
+        cout << "Zmienna b jest mniejsza od zmiennej a";
+    }
+}
+
